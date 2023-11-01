08378067ed342cdad4dbfde3be99430095e9f06d
jagdeep05 <141909556+jagdeep05@users.noreply.github.com>
2023-11-01 19:12:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function processTask() {
+    if (inputBox.value === '') {
+        alert("You must write something");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+function addTask() {
+    processTask();
+}
+
+inputBox.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {
+        processTask();
+    }
+}, false);
+
+listContainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask();
+
+
+
+
+/*const inputBox= document.getElementById("input-box");
+const listContainer= document.getElementById("list-container");
+
+
+function addTask(){
+    if(inputBox.value===''){
+alert("you must write something");
+    }
+    else{
+        let li= document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span= document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    saveData();
+
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+
+showTask();
+*/
